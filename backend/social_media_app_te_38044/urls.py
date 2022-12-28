16a4b0c17b5549d4e256677edbe1f20798ffa341
Crowdbotics <team@crowdbotics.com>
2022-12-28 19:35:00 +0000

    Update admin title

--- backend/social_media_app_te_38044/urls.py
+++ backend/social_media_app_te_38044/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Social Media app Template"
-admin.site.site_title = "Social Media app Template Admin Portal"
-admin.site.index_title = "Social Media app Template Admin"
+admin.site.site_header = "Social Network Template"
+admin.site.site_title = "Social Network Template Admin Portal"
+admin.site.index_title = "Social Network Template Admin"
 
 # swagger
 api_info = openapi.Info(
